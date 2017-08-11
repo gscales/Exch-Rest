64f54695909f30fd36367510c8444457060371e5
Datarumble.Glenscales <gscales@datarumble.com>
2017-08-11 11:05:23 +1000

    update for 2.0 folder pickers

--- Exch-Rest.psm1
+++ Exch-Rest.psm1
@@ -879,6 +879,23 @@ function Get-FolderFromPath{
 	}
 }
 
+function Get-RootMailFolder{
+    param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken
+    )
+    Begin{
+        if($AccessToken -eq $null)
+        {
+              $AccessToken = Get-AccessToken -MailboxName $MailboxName          
+        }   
+        $HttpClient =  Get-HTTPClient($MailboxName)
+        $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
+        $RequestURL =   $EndPoint + "('$MailboxName')/MailFolders/msgfolderroot"
+        return Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName
+    }
+}
+
 function Get-Inbox{
     param( 
         [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
@@ -895,6 +912,23 @@ function Get-Inbox{
         return Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName
     }
 }
+function Get-CalendarFolder{
+    param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken
+    )
+    Begin{
+        if($AccessToken -eq $null)
+        {
+              $AccessToken = Get-AccessToken -MailboxName $MailboxName          
+        }   
+        $HttpClient =  Get-HTTPClient($MailboxName)
+        $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
+        $RequestURL =   $EndPoint + "('$MailboxName')/M"
+        return Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName
+    }
+}
+
 
 function Get-InboxItems{
     param( 
@@ -976,20 +1010,29 @@ function Get-FolderItems{
         [Parameter(Position=3, Mandatory=$false)] [PSCustomObject]$Folder,
         [Parameter(Position=4, Mandatory=$false)] [switch]$ReturnSize,
         [Parameter(Position=5, Mandatory=$false)] [string]$SelectProperties,
-        [Parameter(Position=6, Mandatory=$false)] [string]$Filter
+        [Parameter(Position=6, Mandatory=$false)] [string]$Filter,
+        [Parameter(Position=7, Mandatory=$false)] [string]$Top,
+        [Parameter(Position=8, Mandatory=$false)] [string]$OrderBy,
+        [Parameter(Position=9, Mandatory=$false)] [bool]$TopOnly
     )
     Begin{
         if($AccessToken -eq $null)
         {
               $AccessToken = Get-AccessToken -MailboxName $MailboxName          
         }  
-        if($FolderPath -ne $null)
-        {
+        if(![String]::IsNullorEmpty($FolderPath)){        
             $Folder = Get-FolderFromPath -FolderPath $FolderPath -AccessToken $AccessToken -MailboxName $MailboxName         
         }
         if(![String]::IsNullorEmpty($Filter)){
             $Filter = "`&`$filter=" + $Filter
-        }        
+        }
+        if(![String]::IsNullorEmpty($Orderby)){
+            $OrderBy = "`&`$OrderBy=" + $OrderBy
+        }
+        $TopValue = "1000"    
+        if(![String]::IsNullorEmpty($Top)){
+            $TopValue = $Top
+        }      
         if([String]::IsNullorEmpty($SelectProperties)){
             $SelectProperties = "`$select=ReceivedDateTime,Sender,Subject,IsRead"
         }
@@ -999,12 +1042,16 @@ function Get-FolderItems{
         if($Folder -ne $null)
         {
             $HttpClient =  Get-HTTPClient($MailboxName)
-            $RequestURL =  $Folder.FolderRestURI + "/messages/?" +  $SelectProperties + "`&`$Top=1000" + $Filter
-            write-host $RequestURL
+            $RequestURL =  $Folder.FolderRestURI + "/messages/?" +  $SelectProperties + "`&`$Top=" + $TopValue + $Filter + $OrderBy
+            
             if($ReturnSize.IsPresent){
-                $RequestURL =  $Folder.FolderRestURI + "/messages/?`$select=ReceivedDateTime,Sender,Subject,IsRead`&`$Top=1000`&`$expand=SingleValueExtendedProperties(`$filter=PropertyId%20eq%20'Integer%200x0E08')" + $Filter
+                $PropName = "PropertyId"
+                if($AccessToken.resource -eq "https://graph.microsoft.com"){
+                        $PropName = "Id"
+                }
+                $RequestURL =  $Folder.FolderRestURI + "/messages/?`$select=ReceivedDateTime,Sender,Subject,IsRead`&`$Top=" + $TopValue + "`&`$expand=SingleValueExtendedProperties(`$filter=$PropName%20eq%20'Integer%200x0E08')" + $Filter + $OrderBy
             }
-           
+            write-host $RequestURL
             do{
                 $JSONOutput = Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName
                 foreach ($Message in $JSONOutput.Value) {
@@ -1012,7 +1059,7 @@ function Get-FolderItems{
                     Write-Output $Message
                 }           
                 $RequestURL = $JSONOutput.'@odata.nextLink'
-            }while(![String]::IsNullOrEmpty($RequestURL))     
+            }while(![String]::IsNullOrEmpty($RequestURL) -band (!$TopOnly))     
        } 
    
 
@@ -1400,36 +1447,41 @@ function Get-AllMailboxItems{
     }
 }
 
-function GetExtendedPropList{
+function Get-ExtendedPropList{
         param( 
-        [Parameter(Position=1, Mandatory=$false)] [PSCustomObject]$PropertyList
+        [Parameter(Position=1, Mandatory=$false)] [PSCustomObject]$PropertyList,
+        [Parameter(Position=2, Mandatory=$false)] [psobject]$AccessToken
     )
     Begin{
         $rtString = "";
+        $PropName = "PropertyId"
+        if($AccessToken.resource -eq "https://graph.microsoft.com"){
+            $PropName = "Id"
+        }
         foreach($Prop in $PropertyList){
             if($Prop.PropertyType -eq "Tagged"){
                 if($rtString -eq ""){
-                    $rtString = "(PropertyId%20eq%20'" + $Prop.DataType + "%20" + $Prop.Id + "')"                 
+                    $rtString = "($PropName%20eq%20'" + $Prop.DataType + "%20" + $Prop.Id + "')"                 
                 }
                 else{
-                    $rtString += " or (PropertyId%20eq%20'" + $Prop.DataType + "%20" + $Prop.Id + "')"       
+                    $rtString += " or ($PropName%20eq%20'" + $Prop.DataType + "%20" + $Prop.Id + "')"       
                 }
             }
             else{
                 if($Prop.Type -eq "String"){
                     if($rtString -eq ""){
-                        $rtString = "(PropertyId%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Name%20"+ $Prop.Id + "')"                 
+                        $rtString = "($PropName%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Name%20"+ $Prop.Id + "')"                 
                     }
                     else{
-                        $rtString += " or (PropertyId%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Name%20"+ $Prop.Id + "')"       
+                        $rtString += " or ($PropName%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Name%20"+ $Prop.Id + "')"       
                     }   
                 }
                 else{
                     if($rtString -eq ""){
-                        $rtString = "(PropertyId%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Id%20"+ $Prop.Id + "')"                 
+                        $rtString = "($PropName%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Id%20"+ $Prop.Id + "')"                 
                     }
                     else{
-                        $rtString += " or (PropertyId%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Id%20"+ $Prop.Id + "')"       
+                        $rtString += " or ($PropName%20eq%20'" + $Prop.DataType + "%20{" + $Prop.Guid + "}%20Id%20"+ $Prop.Id + "')"       
                     }                    
                 }
             }
@@ -1517,7 +1569,7 @@ function Get-AllMailFolders{
             $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
             $RequestURL =  $EndPoint + "('$MailboxName')/MailFolders/msgfolderroot/childfolders/?`$Top=1000"
             if($PropList -ne $null){
-               $Props = GetExtendedPropList -PropertyList $PropList
+               $Props = Get-ExtendedPropList -PropertyList $PropList -AccessToken $AccessToken
                $RequestURL += "`&`$expand=SingleValueExtendedProperties(`$filter=" + $Props + ")"
                Write-Host $RequestURL
             }
@@ -1559,7 +1611,7 @@ function Get-AllChildFolders{
             $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
             $RequestURL =   $Folder.FolderRestURI + "/childfolders/?`$Top=1000"
             if($PropList -ne $null){
-               $Props = GetExtendedPropList -PropertyList $PropList
+               $Props = Get-ExtendedPropList -PropertyList $PropList -AccessToken $AccessToken
                $RequestURL += "`&`$expand=SingleValueExtendedProperties(`$filter=" + $Props + ")"
             }
             do{
@@ -2450,6 +2502,201 @@ function Get-DefaultOneDriveRootItems{
     }
 }
 
+<<<<<<< HEAD
+function Invoke-EnumOneDriveFolders{
+    param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken,
+        [Parameter(Position=2, Mandatory=$false)] [String]$DriveRESTURI,
+        [Parameter(Position=3, Mandatory=$false)] [String]$FolderPath
+    )
+    Begin{
+        if($AccessToken -eq $null)
+        {
+              $AccessToken = Get-AccessToken -MailboxName $MailboxName          
+        }  
+          $HttpClient =  Get-HTTPClient($MailboxName)
+          if([String]::IsNullOrEmpty($DriveRESTURI)){
+               $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
+               $RequestURL =  $EndPoint + "('$MailboxName')/drive/root/children?`$filter folder ne null`&`$Top=1000"
+           }
+           else{
+                 $RequestURL =  $DriveRESTURI + "/children?`$filter folder ne null`&`$Top=1000"
+           }            
+           do{
+                  $JSONOutput = Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName   
+                  foreach ($Item in $JSONOutput.value) {
+                       Add-Member -InputObject $Item -NotePropertyName DriveRESTURI -NotePropertyValue (((Get-EndPoint -AccessToken $AccessToken -Segment "users") + "('$MailboxName')/drive") + "/items('" + $Item.Id + "')")
+                       Add-Member -InputObject $Item -NotePropertyName Path -NotePropertyValue ("\" + $Item.name)
+                           if([bool]($Item.PSobject.Properties.name -match "folder")){
+                               write-output $Item 
+                               if($Item.folder.childCount -gt 0){
+                                   Invoke-EnumChildFolders -DriveRESTURI $Item.DriveRESTURI -MailboxName $MailboxName -AccessToken $AccessToken -Path $Item.Path
+                               }
+                           } 
+                  }           
+                  $RequestURL = $JSONOutput.'@odata.nextLink'                  
+        }while(![String]::IsNullOrEmpty($RequestURL))      
+
+    }
+}
+function Invoke-EnumChildFolders{
+    param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken,
+        [Parameter(Position=2, Mandatory=$false)] [String]$DriveRESTURI,
+        [Parameter(Position=3, Mandatory=$false)] [String]$Path        
+    )
+    Begin{
+        if($AccessToken -eq $null)
+        {
+              $AccessToken = Get-AccessToken -MailboxName $MailboxName          
+        }  
+            $HttpClient =  Get-HTTPClient($MailboxName)
+          if([String]::IsNullOrEmpty($DriveRESTURI)){
+               $EndPoint =  Get-EndPoint -AccessToken $AccessToken -Segment "users"
+               $RequestURL =  $EndPoint + "('$MailboxName')/drive/root:" + $FolderPath + ":/children?`$filter folder ne null`&`$Top=1000"
+           }
+           else{
+                 $RequestURL =  $DriveRESTURI + "/children?`$filter folder ne null`&`$Top=1000"
+           }            
+           $pc =0;
+           do{
+               $pc++
+               #write-host "Page " + $pc
+                  $JSONOutput = Invoke-RestGet -RequestURL $RequestURL -HttpClient $HttpClient -AccessToken $AccessToken -MailboxName $MailboxName   
+                  foreach ($Item in $JSONOutput.value) {
+                       Add-Member -InputObject $Item -NotePropertyName DriveRESTURI -NotePropertyValue (((Get-EndPoint -AccessToken $AccessToken -Segment "users") + "('$MailboxName')/drive") + "/items('" + $Item.Id + "')")
+                       Add-Member -InputObject $Item -NotePropertyName Path -NotePropertyValue ($Path + "\" + $Item.name)
+                           if([bool]($Item.PSobject.Properties.name -match "folder")){
+                               write-output $Item 
+                               if($Item.folder.childCount -gt 0){
+                                   Invoke-EnumChildFolders -DriveRESTURI $Item.DriveRESTURI -MailboxName $MailboxName -AccessToken $AccessToken -Path $Item.Path
+                               }
+                           } 
+                  }           
+                  $RequestURL = $JSONOutput.'@odata.nextLink'                  
+        }while(![String]::IsNullOrEmpty($RequestURL))       
+
+  
+
+    }
+}
+
+function Invoke-FolderPicker{
+        param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$rootFolder,
+        [Parameter(Position=2, Mandatory=$false)] [psObject]$Folders,
+        [Parameter(Position=3, Mandatory=$false)] [string]$pickerType,
+        [Parameter(Position=4, Mandatory=$false)] [switch]$ShowFolderSize
+        )
+    Begin{
+       
+       [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+       [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") 
+       $Treeinfo = @{}
+       $TNRoot = new-object System.Windows.Forms.TreeNode("Root")
+       $TNRoot.Name = "Mailbox"
+       $TNRoot.Text = "Mailbox - " + $MailboxName
+       foreach($ffFolder in $Folders){   
+		#Process folder here
+        switch($pickerType){
+            "onedrive" {
+                $ParentFolderId = $ffFolder.parentReference.Id
+                $folderName = $ffFolder.Name
+             }
+            "mail" {
+                $ParentFolderId = $ffFolder.parentFolderId
+		$folderName = $ffFolder.displayName
+		if($ShowFolderSize){
+		    $folderName = $ffFolder.displayName + " (" + [math]::round($ffFolder.singleValueExtendedProperties[0].value /1Mb, 0)   + " mb)"
+		}                
+             }
+        }   
+		$TNChild = new-object System.Windows.Forms.TreeNode($ffFolder.Name)
+		$TNChild.Name = $folderName
+		$TNChild.Text = $folderName
+		$TNChild.tag = $ffFolder
+   
+        
+		if ($ParentFolderId -eq $rootFolder.Id){
+			[void]$TNRoot.Nodes.Add($TNChild) 
+			$Treeinfo.Add($ffFolder.Id.ToString(),$TNChild)
+		}
+		else{
+			$pfFolder = $Treeinfo[$ParentFolderId]
+			[void]$pfFolder.Nodes.Add($TNChild)
+			if ($Treeinfo.ContainsKey($ffFolder.Id) -eq $false){
+				$Treeinfo.Add($ffFolder.Id,$TNChild)
+			}
+		}
+    } 
+    $Script:clickedFolder = $null
+    $objForm = New-Object System.Windows.Forms.Form 
+    $objForm.Text = "Folder Select Form"
+    $objForm.Size = New-Object System.Drawing.Size(600,600) 
+    $objForm.StartPosition = "CenterScreen"
+    $tvTreView1 = new-object System.Windows.Forms.TreeView
+    $tvTreView1.Location = new-object System.Drawing.Size(1,1) 
+    $tvTreView1.add_DoubleClick({
+        $Script:clickedFolder = $this.SelectedNode.tag
+        $objForm.Close()
+    })
+    $tvTreView1.size = new-object System.Drawing.Size(580,580) 
+    $tvTreView1.Anchor = "Top,left,Bottom"
+    [void]$tvTreView1.Nodes.Add($TNRoot) 
+    $objForm.controls.add($tvTreView1)
+    [void] $objForm.ShowDialog()
+    return ,$Script:clickedFolder
+
+    }
+}
+
+function Invoke-OneDriveFolderPicker{
+        param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken
+    )
+    Begin{
+       $rootFolder = Get-DefaultOneDrive -AccessToken $AccessToken -MailboxName $MailboxName
+       $Folders = Invoke-EnumOneDriveFolders -MailboxName $MailboxName -AccessToken $AccessToken
+       Invoke-FolderPicker -MailboxName $MailboxName -Folders $Folders -rootFolder $rootFolder -pickerType onedrive
+}
+}
+ 
+function Invoke-MailFolderPicker{
+        param( 
+        [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
+        [Parameter(Position=1, Mandatory=$false)] [psobject]$AccessToken,
+        [Parameter(Position=2, Mandatory=$false)] [switch]$ShowFolderSize
+
+    )
+    Begin{       
+       
+       $rootFolder = Get-RootMailFolder -AccessToken $AccessToken -MailboxName $MailboxName
+       if($ShowFolderSize){
+           $PropList = @()
+           $FolderSizeProp = Get-TaggedProperty -Id "0x0E08" -DataType Long
+           $PropList += $FolderSizeProp
+           $Folders = Get-AllMailFolders -MailboxName $MailboxName -AccessToken $AccessToken -PropList $PropList
+        }
+        else{
+            $Folders = Get-AllMailFolders -MailboxName $MailboxName -AccessToken $AccessToken
+        }   
+
+      
+       if($ShowFolderSize){
+              Invoke-FolderPicker -MailboxName $MailboxName -Folders $Folders -rootFolder $rootFolder -pickerType mail -ShowFolderSize
+       }
+       else{
+              Invoke-FolderPicker -MailboxName $MailboxName -Folders $Folders -rootFolder $rootFolder -pickerType mail   
+       }
+    }
+}    
+
+=======
+>>>>>>> fb6f8121b5985c6523d82df7a3cbd814d5b9f5ce
 function Get-OneDriveChildren{
     param( 
         [Parameter(Position=0, Mandatory=$true)] [string]$MailboxName,
