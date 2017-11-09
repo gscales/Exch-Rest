f236dd5dadc73b94b36294cf22412b92fa32027f
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-11-09 10:50:17 +0100

    Added new internal commands

---
+++ internal/functions/security/Protect-String.ps1
@@ -0,0 +1,38 @@
+﻿function Protect-String
+{
+<#
+	.SYNOPSIS
+		Uses DPAPI to encrypt strings.
+	
+	.DESCRIPTION
+		Uses DPAPI to encrypt strings.
+	
+	.PARAMETER String
+		The string to encrypt.
+	
+	.EXAMPLE
+		PS C:\> Protect-String -String $secret
+	
+		Encrypts the content stored in $secret and returns it.
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline = $true)]
+		[string[]]
+		$String
+	)
+	
+	begin
+	{
+		Add-Type -AssemblyName System.Security -ErrorAction Stop
+	}
+	process
+	{
+		foreach ($item in $String)
+		{
+			$stringBytes = [Text.Encoding]::UTF8.GetBytes($item)
+			$encodedBytes = [System.Security.Cryptography.ProtectedData]::Protect($stringBytes, $null, 'CurrentUser')
+			[System.Convert]::ToBase64String($encodedBytes) | ConvertTo-SecureString -AsPlainText -Force
+		}
+	}
+}
---
+++ internal/functions/security/Unprotect-String.ps1
@@ -0,0 +1,40 @@
+﻿function Unprotect-String
+{
+<#
+	.SYNOPSIS
+		Uses DPAPI to decrypt strings.
+	
+	.DESCRIPTION
+		Uses DPAPI to decrypt strings.
+		Designed to reverse encryption applied by Protect-String
+	
+	.PARAMETER String
+		The string to decrypt.
+	
+	.EXAMPLE
+		PS C:\> Unprotect-String -String $secret
+	
+		Decrypts the content stored in $secret and returns it.
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(ValueFromPipeline = $true)]
+		[System.Security.SecureString[]]
+		$String
+	)
+	
+	begin
+	{
+		Add-Type -AssemblyName System.Security -ErrorAction Stop
+	}
+	process
+	{
+		foreach ($item in $String)
+		{
+			$cred = New-Object PSCredential("irrelevant", $item)
+			$stringBytes = [System.Convert]::FromBase64String($cred.GetNetworkCredential().Password)
+			$decodedBytes = [System.Security.Cryptography.ProtectedData]::Unprotect($stringBytes, $null, 'CurrentUser')
+			[Text.Encoding]::UTF8.GetString($decodedBytes)
+		}
+	}
+}
